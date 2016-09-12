1dc33d170b33b4c76d6e467277400b173e6388b4
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-13 02:00:20 +0300

    Fetch object

---
+++ fetch-object.php
@@ -0,0 +1,28 @@
+<?php
+
+// 1. Connection
+try {
+	$pdo = new PDO('mysql:host=localhost;dbname=chinook', 'root', '');
+} catch (PDOException $e) {
+	die($e->getMessage());
+}
+
+$artistId = 21;
+
+$sql = "select * from Album where ArtistId = :artistId";
+
+// 2. Statement
+
+$stmt = $pdo->prepare($sql);
+
+// 3. Execute
+
+$stmt->bindParam(':artistId', $artistId);
+
+$stmt->execute();
+
+// 4. Fetch Data
+
+$albums = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+var_dump($albums);
